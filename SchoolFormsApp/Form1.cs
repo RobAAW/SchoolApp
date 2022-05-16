d9ad5066ddc78e2a82f7d57e327dc16d1754387e
AaronWard <r.aaronward@gmail.com>
2022-05-16 09:40:46 -0400

    Add project files.

---
+++ SchoolFormsApp/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SchoolLibrary;
+
+namespace SchoolFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPushToTest_Click(object sender, EventArgs e)
+        {
+            School testSchool = new School();
+            testSchool.Name = textName.Text;
+            testSchool.Address = textAddress.Text;
+            testSchool.City = textCity.Text;
+            testSchool.State = textState.Text;
+            testSchool.Zip = textZip.Text;
+            testSchool.PhoneNumber = textPhone.Text;
+            try
+            {
+                testSchool.TwitterAddress = textTwitterAddress.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            MessageBox.Show(testSchool.ToString());
+
+        }
+
+        private void btnTestTeacher_Click(object sender, EventArgs e)
+        {
+            var teacher = new Teacher();
+            var gp = teacher.ComputeGradeAverage();
+            MessageBox.Show("The teacher's grade average is " + gp);
+        }
+
+        private void btnTestStudent_Click(object sender, EventArgs e)
+        {
+            var student = new Student();
+            var gp = student.ComputeGradeAverage();
+            MessageBox.Show("The student's grade average is " + gp);
+        }
+    }
+}
---
+++ SchoolLibrary/ScoreUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class ScoreUtility
+    {
+        public static IScored BestOfTwo(IScored AssignmentOne, IScored AssignmentTwo)
+        {
+            var scoreOne = AssignmentOne.Score / AssignmentOne.MaximumScore;
+            var scoreTwo = AssignmentTwo.Score / AssignmentTwo.MaximumScore;
+
+            if (scoreOne > scoreTwo)
+            {
+                return AssignmentOne;
+            }
+            else
+            {
+                return AssignmentTwo;
+            }
+        }
+    }
+}
